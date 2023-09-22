109b1ca2aaeeda3eb560b7984b6a1125077cf804
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-09-22 19:57:49 +0100

    Update page.tsx

--- app/user/[productId]/page.tsx
+++ app/user/[productId]/page.tsx
@@ -13,18 +13,24 @@ export default async function UserPage({
     const user = await WhopAPI.user({ headers }).GET("/me", {}); // This will fetch the user's information
 
     return (
-      <main>
-        <div className="pt-5 space-y-2">
-          <h2>Content</h2>
-          <ul>
-            <li><a href="https://docs.google.com/spreadsheets/d/12I4lEgrCwYnBEv4pf8O1FskS6tpRM-D3FFzo9ajG6gI/edit#gid=1753519888" target="_blank">Content Planner</a></li>
-          </ul>>
+      <div className="space-y-4">
+        {/* New Section */}
+        <div className="bg-gray-100 p-4 rounded-lg">
+          <h2 className="text-xl font-bold mb-2">Useful Links</h2>
+          <ul className="space-y-2">
+            <li><a href="#" className="text-blue-500 underline">Link 1</a></li>
+            <li><a href="#" className="text-blue-500 underline">Link 2</a></li>
+            <li><a href="#" className="text-blue-500 underline">Link 3</a></li>
+          </ul>
         </div>
 
+        {/* Existing Section */}
         <div className="pt-5 space-y-2">
+          <p>Username: {user.data?.username}</p>
           <p>Email: {user.data?.email}</p>
+          <OpenButton />
         </div>
-      </main>
+      </div>
     );
   } catch (error) {
     return (
