1269caa51251c85c83b7e46aa41a0b96f19c8a0e
Colin McDermott <colin.m.mcdermott@googlemail.com>
2023-09-22 18:58:05 +0100

    1

--- app/user/[productId]/page.tsx
+++ app/user/[productId]/page.tsx
@@ -1,40 +1,43 @@
+import { useState } from 'react';
 import { validateToken, WhopAPI } from "@whop-apps/sdk";
 import { headers } from "next/headers";
 import OpenButton from "@/components/OpenButton";
+import { fetchAndParseRSS } from '../utils/rssFeed';
 
 export default async function UserPage({
   params,
 }: {
   params: { productId: string };
 }) {
-  try {
-    await validateToken({ headers }); // This will ensure only authenticated users can access this page
-
-    const user = await WhopAPI.user({ headers }).GET("/me", {}); // This will fetch the user's information
-
-    return (
-      <div className="pt-5 space-y-2">
-        <p>Username: {user.data?.username}</p>
-        <p>Email: {user.data?.email}</p>
-        <p>Product ID: {params.productId}</p>
-        <OpenButton />
-      </div>
-    );
-  } catch (error) {
-    return (
-      <div className="h-screen flex flex-col items-center justify-center">
-        <p>
-          If you are the developer, make sure you are developing in the iFrame.
-          For more details, head {""}
-          <a
-            className="underline text-blue-500"
-            href="https://apps.whop.com/apps/environment"
-            target="_blank"
-          >
-            here
-          </a>
-        </p>
-      </div>
-    );
+  const [rssUrls, setRssUrls] = useState<string[]>([]);
+  
+  async function handleFetchRSS() {
+    try {
+      await validateToken({ headers });  // Ensure only authenticated users can access this page
+      const user = await WhopAPI.user({ headers }).GET("/me", {});  // Fetch the user's information
+      const affiliateUrls = await fetchAndParseRSS(user.data?.username);
+      setRssUrls(affiliateUrls);
+    } catch (error) {
+      console.error(error);
+      // Handle error
+    }
   }
+
+  return (
+    <div className="pt-5 space-y-2">
+      <p>Product ID: {params.productId}</p>
+      <OpenButton />
+      <button onClick={handleFetchRSS}>Fetch RSS</button>
+      {rssUrls.length > 0 && (
+        <div>
+          <h3>Affiliate URLs:</h3>
+          <ul>
+            {rssUrls.map((url, index) => (
+              <li key={index}>{url}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
